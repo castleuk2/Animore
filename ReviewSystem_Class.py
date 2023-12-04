c5bd89aca77e46aa54274c41db7fac77b545b939
jongseokyoo <150095668+jongseokyoo@users.noreply.github.com>
2023-12-05 00:28:43 +0900

    Update ReviewSystem_Class.py

--- ReviewSystem_Class.py
+++ ReviewSystem_Class.py
@@ -29,17 +29,6 @@ class ReviewSystem:
         else:
             print(f"{center_name} 병원 리스트에서 해당 병원을 찾을 수 없습니다2.")
 
-    def view_reviews(self, center_name):
-        if center_name in self.centers:
-            reviews = self.centers[center_name]['reviews']
-            if not reviews:
-                print("리뷰가 없습니다.")
-            else:
-                for i, review in enumerate(reviews, 1):
-                    print(f"리뷰 {i}: {review['리뷰']} (평점: {review['평점']})")
-        else:
-            print(f"{center_name} 병원 리스트에서 해당 병원을 찾을 수 없습니다.")
-
     def write_review_interaction(self,center_list):
         center_name = input("\n리뷰를 작성하시겠습니까? 다녀온 병원 이름을 입력하세요. (나가기: 'exit'): ")
         if center_name.lower() == 'exit':
@@ -48,7 +37,3 @@ class ReviewSystem:
         rating = int(input("평점을 매겨주세요. (0-5): "))
         review = input("리뷰를 작성해주세요. : ")
         self.add_review( center_list, center_name, rating, review)
-
-        view_reviews = input("이 센터의 모든 리뷰를 보시겠습니까? (Y/N): ")
-        if view_reviews.lower() == 'y':
-            self.view_reviews(center_name)
