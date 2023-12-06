ce981b7ceb1bdfd3bf523a972867f469b6be34a9
serakim-png <69424035+serakim-png@users.noreply.github.com>
2023-12-06 16:52:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,28 +20,26 @@ def main():
         result = save_list.filter_data_by_region(find_address)
         center_list = result.values.tolist()
         print(center_list)
-        address1 = real_address(result)
-        if not result.empty: #결과값이 비지 않았다면 전화번호만 따로 저장하는 부분.
+        if not result.empty: 
+            address1 = real_address(result)
             phone_numbers = phone_number(result)
             facility_names = facility_name(result)
         
-        latitude = []
-        longitude =[]
-    
-        for i in address1:
-            latitude.append(geocoding(i)[0])
-            longitude.append(geocoding(i)[1])
-        hospital_locations = []
-        for lat, lon in zip(latitude, longitude):
-            hospital_locations.append([lat, lon])
-            
-    
-        m = folium.Map(location=[37.53897093698831, 127.05461953077439], 
-                   zoom_start=12, 
-                   ) #ㅡㅐㅡㅐ
+        hospital_locations = get_locations(address1)
+        
+        m = folium.Map(location=geocoding(customer1.address), 
+                zoom_start=14, 
+                )
+        folium.Marker(
+        location=geocoding(customer1.address),
+        popup="현재 나의 위치",
+        icon=folium.Icon(color="purple", icon="home"),).add_to(m)
         for i in range(len(hospital_locations)):
-            folium.Marker(location=hospital_locations[i], tooltip = f"{facility_names[i]} {phone_numbers[i]}").add_to(m)
-        m.save('C:\\Users\\monog\\OneDrive\\바탕 화면\\Python_Project\\Animore\\momo.html') #다 안뜨는 오류...
+            folium.Marker(location=hospital_locations[i],popup=f"{haversine(hospital_locations[i],geocoding(customer1.address),unit = 'km'):.2f} km", tooltip = f"{facility_names[i]} {phone_numbers[i]}").add_to(m)
+        m.save('location.html')
+        webbrowser.open_new('location.html')
+        
+        
         
         customer1.appoint_Center()
         appoint_center = Center()
